e38eceeb941e02f94e6e520468c42051d95ecdd1
msedou <korda.daaba@gmail.com>
2026-03-31 09:39:49 +0000

    feat: add employee db context

--- services/employee-service/Infrastructure/Persistence/EmployeeDbContext.cs
+++ services/employee-service/Infrastructure/Persistence/EmployeeDbContext.cs
@@ -1,12 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using EmployeeService.Domain.Entities;
 
 namespace EmployeeService.Infrastructure.Persistence;
 
 public class EmployeeDbContext : DbContext
 {
-          public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
-              : base(options) { }
+    public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+        : base(options) { }
 
-          public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<Employee> Employees => Set<Employee>();
 }
