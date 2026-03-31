b4e9595e787835fba640792987dfe0310302d2b6
msedou <korda.daaba@gmail.com>
2026-03-31 11:56:46 +0000

    feat: modify Emplyee entiti to add Phone number propertise

--- services/employee-service/Domain/Entities/Employee.cs
+++ services/employee-service/Domain/Entities/Employee.cs
@@ -1,13 +1,12 @@
-public class Employee {
-          public Guid Id { get; set; }
+public class Employee
+{
+    public Guid Id { get; set; }
 
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
     public string Email { get; set; } = default!;
-
+    public string PhoneNumber { get; set; } = default!;
     public decimal BaseSalary { get; set; }
-
     public DateTime HireDate { get; set; }
-
     public bool IsActive { get; set; } = true;
 }
