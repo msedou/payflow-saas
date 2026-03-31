9440e26a358040afb591db96f203cb5ff53672e3
msedou <korda.daaba@gmail.com>
2026-03-31 09:44:23 +0000

    feat: bug fix

--- services/employee-service/API/Controllers/EmployeeController.cs
+++ services/employee-service/API/Controllers/EmployeeController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using EmployeeService.Application.DTOs;
 using EmployeeService.Infrastructure.Persistence;
-using EmployeeService.Domain.Entities;
 
 namespace EmployeeService.API.Controllers;
 
