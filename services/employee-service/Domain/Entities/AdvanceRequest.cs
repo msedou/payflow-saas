096f77b16962668119f018306796fb3b49d19d44
msedou <korda.daaba@gmail.com>
2026-03-31 11:25:10 +0000

    feat: add advance request entitie

--- services/employee-service/Domain/Entities/AdvanceRequest.cs
+++ services/employee-service/Domain/Entities/AdvanceRequest.cs
@@ -8,7 +8,7 @@ public class AdvanceRequest
           public Guid Id { get; set; }
           public Guid EmployeeId { get; set; }
           public decimal Amount { get; set; }
-          public RequestStatus Status { get; set; } // Enum: Pending, Approved, etc.
+          public RequestStatus Status { get; set; }
           public DateTime RequestDate { get; set; } = DateTime.UtcNow;
           public DateTime? PaymentDate { get; set; }
           public string TransactionReference { get; set; }
