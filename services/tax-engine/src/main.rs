bf82a3a5569863f45e47c23a08f1957e59a1f687
msedou <korda.daaba@gmail.com>
2026-04-01 12:13:50 +0000

    feat: add main projet

--- services/tax-engine/src/main.rs
+++ services/tax-engine/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod domain;
+mod application;
+mod infrastructure;
+mod api;
+
+use infrastructure::http::server::run_server;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("🚀 Tax Engine running on http://localhost:8081");
+    run_server().await
 }
